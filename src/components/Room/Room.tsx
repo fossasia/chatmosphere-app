08e3a9ef67964a5f1ada311dce4dea4104516f4d
Shubham Upreti <shubham.upreti@gmail.com>
2022-04-28 16:17:30 +0530

    change image fetching

--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect } from 'react';
-import styled from 'styled-components';
-import { panOptions } from '../PanWrapper/panOptions';
+import React, { useEffect, useState } from "react"
+import styled from "styled-components"
+import { panOptions } from "../PanWrapper/panOptions"
 
 /* fixed size won't work, because when scale is 1 there will be room to pan; but the plugin won't allow it because scale is 1. 
 the fix is to set the size of the react-transform-component and react-transform-element exlusively (see App.css) */
 
-
-const RoomContainer = styled.div`
-  width:${panOptions.room.size.x}px;height:${panOptions.room.size.y}px;
+const RoomContainer = styled.div<{ bg: string }>`
+  width: ${panOptions.room.size.x}px;
+  height: ${panOptions.room.size.y}px;
   box-sizing: border-box;
-  display:block;
-  background:${props => props.background};
+  display: block;
+  background: ${(props) => (props.bg ? "url(props.bg)" : "brown")};
 `
 
 // const Background = styled.div`
@@ -20,22 +20,34 @@ const RoomContainer = styled.div`
 //   height:100%;
 // `
 
-export const Room:React.FC = ({children}) => {
-  const [background, setBackground] = useEffect(() => {
-    try {
-      const data = fetch(`https://api.eventyay.com/v1/events/350bf76b/video-stream`).then(res => res.json())
-      const bg = data['data']['attributes']['bg-img-url']
-      setBackground(bg)
-    } catch (err) {
-      console.error(err)
+export const Room: React.FC = ({ children }) => {
+  const [background, setBackground] = useState("")
+  useEffect(() => {
+    const getBg = async () => {
+      try {
+        const data = await fetch(
+          `https://api.eventyay.com/v1/events/350bf76b/video-stream`,
+          {
+            headers: {
+              Accept: "application/vnd.api+json",
+            },
+          },
+        ).then((res) => res.json())
+        console.log(data)
+        const bg = data["data"]["attributes"]["bg-img-url"]
+        console.log(bg)
+        setBackground(bg)
+      } catch (err) {
+        console.error("Not able to fetch background", err)
+      }
     }
+    getBg()
   }, [])
 
   return (
-    <RoomContainer background>
+    <RoomContainer bg={background}>
       {/* <Background /> */}
       {children}
     </RoomContainer>
   )
 }
-
